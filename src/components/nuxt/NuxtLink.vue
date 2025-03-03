fe5595c38500d2290d3764be86c88b67a54d0787
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-03-03 19:01:22 +0900

    Update NuxtLink.vue

--- src/components/nuxt/NuxtLink.vue
+++ src/components/nuxt/NuxtLink.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <router-link :to="to">
+  <router-link :to="to" active-class="" exact-active-class="" aria-current="false">
     <slot />
   </router-link>
 </template>
