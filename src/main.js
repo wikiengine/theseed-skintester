80eb697d5ee092098f602e6977f40f147d103de7
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-03-03 12:36:36 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Nuxt from './components/nuxt/Nuxt.vue'
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.component("nuxt", Nuxt);
 app.mount("#app");
