fb7b53423e8b228623b9b8169b2fb02182fea8d3
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-03-03 14:12:16 +0900

    Create NuxtLink.vue

---
+++ src/components/nuxt/NuxtLink.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <router-link :to="to" class="custom-link">
+    <slot />
+  </router-link>
+</template>
